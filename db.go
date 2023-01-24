35cdcd67d436e75704179d25e2a5a81fd6d074f4
Vagner de Araujo <vagnerdearaujo@gmail.com>
2023-01-24 16:01:06 -0300

    Atualização do db.go
    Solicitei ao GPT que melhorasse a conexão ao banco criando uma connection string para cada tipo de banco.

--- db.go
+++ db.go
@@ -16,7 +16,18 @@ func Connect() (*sql.DB, error) {
     if err != nil {
         return nil, err
     }
-    db, err := sql.Open(dbConfig.Type, fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Database))
+    var connString string
+    switch dbConfig.Type {
+    case "sqlserver":
+        connString = fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;database=%s;", dbConfig.Host, dbConfig.User, dbConfig.Password, dbConfig.Port, dbConfig.Database)
+    case "firebird":
+        connString = fmt.Sprintf("sysdba:masterkey@%s:%s/%s", dbConfig.Host, dbConfig.Port, dbConfig.Database)
+    case "mysql":
+        connString = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
+    case "postgres":
+        connString = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s", dbConfig.Host, dbConfig.Port, dbConfig.User, dbConfig.Password, dbConfig.Database)
+    }
+    db, err := sql.Open(dbConfig.Type, connString)
     if err != nil {
         return nil, err
     }
